e4b6493c8777c158811e169c663e444acf3cad61
Swashata Ghosh <swashata@wpquark.com>
2017-08-31 04:00:28 +0000

    Project init
    Take all jobs, gitlab ci and sonar stuff

---
+++ tests/phpunit/testcases/sample-test.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Wpq_Wp_Update
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
