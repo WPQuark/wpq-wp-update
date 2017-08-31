3e1a96cd4daa611c47bd2c0c053dab8f263802a9
Swashata Ghosh <swashata@wpquark.com>
2017-08-31 03:44:17 +0000

    Initial commit

---
+++ wpq-wp-update.php
@@ -0,0 +1,30 @@
+<?php
+/*
+Plugin Name: WP Update Server
+Description: WP Update Server for your private & commercial Plugins
+Plugin URI: http://wpquark.com
+Author: Swashata
+Author URI: http://swashata.me
+Version: 0.0.1
+License: GPL3
+Text Domain: wpq-wp-update
+Domain Path: translations
+*/
+
+/*
+
+    Copyright (C) 2017  Swashata  swashata@wpquark.com
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
